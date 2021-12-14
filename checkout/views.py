c9e02509d1aaa6227aa4a2f4074d8c82ff8ae1a8
MagQ98 <maggieq.quinlan@gmail.com>
2021-12-14 18:24:38 +0000

    Added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51K6f6ADBRKWE3Pff3n5DMGSqulnBav6Okjh4YZweRaj0MRdRxGGnzPRFG7nVaG1iurNGZ1J3Qw0GkORjFSq5p8cg00riAMobKP',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
